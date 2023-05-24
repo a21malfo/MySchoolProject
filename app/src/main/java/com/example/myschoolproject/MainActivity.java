58a0e52e19178c377c18a604bde2d7d1f893fd15
a21malfo <a21@malfo@student.his.se>
2023-05-24 07:20:04 +0200

    Add a recyclerview and a button

--- app/src/main/java/com/example/myschoolproject/MainActivity.java
+++ app/src/main/java/com/example/myschoolproject/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.myschoolproject;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private ArrayList<Dog> dogs;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter dogAdapter;
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a21malfo";
+    private final String JSON_FILE = "dog.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
+
+        // Lookup the recyclerview in activity layout
+        recyclerView = findViewById(R.id.recyclerView);
+        dogs = new ArrayList<Dog>();
+        // Create adapter passing in the sample user data
+        dogAdapter = new RecyclerViewAdapter(dogs);
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.setAdapter(dogAdapter);
+        // Set layout manager to position the items
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        //mountains = new Gson().fromJson(json, new TypeToken<ArrayList<Mountain>>(){}.getType());
+        ArrayList<Dog> data = new Gson().fromJson(json, new TypeToken<ArrayList<Dog>>(){}.getType());
+        dogs.addAll(data);
+
+
+        dogAdapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/com/example/myschoolproject/RecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package com.example.myschoolproject;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Dog> dogs
+            ;
+
+    public RecyclerViewAdapter(List<Dog> dogs) {
+        this.dogs = dogs;
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+
+        public TextView name;
+
+
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+            name = itemView.findViewById(R.id.title);
+
+        }
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.json_data, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText(this.dogs.get(position).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.dogs.size();
+    }
+}
